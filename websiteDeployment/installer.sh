9dcd3fe453c1747627a04186fc340483dd9526c3
tmcdonagh <tmcdonagh12@gmail.com>
2018-02-27 11:19:54 -0600

    Added better dialog options

--- websiteDeployment/installer.sh
+++ websiteDeployment/installer.sh
@@ -8,3 +8,12 @@ if [ $? == 0 ]
 then
   sudo docker build -t websiteoverview .
 fi
+dialog \
+  --backtitle "WebsiteOverview Installer" \
+  --title "Installation" \
+  --yesno "\nRun WebsiteOverview?" 10 30
+echo $?
+if [ $? == 0 ]
+then 
+  sudo docker run -d -p 80:80 websiteoverview
+fi
