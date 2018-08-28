5f9e23c10740f1b0cc13f94dd1b0a24aecc2dc5d
tom <tmcdonagh12@gmail.com>
2018-08-28 18:36:27 -0500

    Added some phrases

--- discordBot/bot.js
+++ discordBot/bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 var auth = require('./auth.json');
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("ready");
 });
 
 client.on("message", (message) => {
@@ -12,8 +12,33 @@ client.on("message", (message) => {
     message.channel.send("pong!");
   }
   if(message.content.startsWith("lmao")){
-    message.channel.send("lmao");
+    message.channel.send("Lmao");
   }
+  if(
+    message.content.startsWith("Thx Lmao Bot") ||
+    message.content.startsWith("Thanks asshole") ||
+    message.content.startsWith("Thx bb") ||
+    message.content.startsWith("thx bb")
+
+  ){
+    message.channel.send("np");
+  }
+  if(
+    message.content.startsWith("niberius") ||
+    message.content.startsWith("Niberius")
+
+  ){
+    message.channel.send("wasus");
+  }
+  if(
+    message.content.startsWith("le mao") ||
+    message.content.startsWith("Le mao")
+  ){
+
+    message.channel.send("https://imgur.com/a/pjRYfO0");
+
+  }
+
 
 });
 
