45abf699252acc6c775754f4bb36d8173dfb6734
tom <tmcdonagh12@gmail.com>
2018-12-05 17:26:33 -0600

    Sped up updater to 1 second from 2 to match cpuCheck

--- clouddb/memCheck.sh
+++ clouddb/memCheck.sh
@@ -8,6 +8,6 @@ do
   time=$(TZ='America/Chicago' date +"%a %b %d %I:%M:%S %p")
   echo "INSERT INTO mem (free, available, total, time) VALUES ('$memFree','$memAvailable','$memTotal','$time')" | mysql -uroot -ptest clouddb
   echo "DELETE FROM mem WHERE ID NOT IN ( SELECT ID FROM ( SELECT ID FROM mem ORDER BY ID DESC LIMIT 50 ) del );" | mysql -ptest clouddb
-  sleep 2
+  sleep 1
 
 done
