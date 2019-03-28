bcccdf0e66cc02024abc809808751bad20d5db11
tom <tmcdonagh12@gmail.com>
2019-03-28 11:24:06 -0500

    added npm and bootstrap to dependencies

--- websiteDeployment/installer.sh
+++ websiteDeployment/installer.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-# Asks whether or not to install
-dialog \
-  --backtitle "WebsiteOverview Installer" \
-  --title "Installation" \
-  --yesno "\nInstall WebsiteOverview?" 10 30
-if [ $? == 0 ]
-then
-  sudo docker build -t websiteoverview .
-else
-  exit 1
-fi
-
-# Asks whether or not to run previously installed container
-dialog \
-  --backtitle "WebsiteOverview Installer" \
-  --title "Installation" \
-  --yesno "\nRun WebsiteOverview?" 10 30
-if [ $? == 0 ]
-then 
-  port=$(dialog --inputbox "What port should 80 be set to?" 10 25 --output-fd 1)
-  sudo docker run --name=web --restart=always -d -p $port:80 websiteoverview
-fi
+sudo docker build -t websiteoverview .
+port=80
+sudo docker run --name=web --restart=always -d -p $port:80 websiteoverview
