9d40d8f8f9aa7d6bc7c0fb24e4810bf0509085ce
tmcdonagh <tmcdonagh12@gmail.com>
2019-08-07 17:57:55 -0500

    Fixed Typo

--- clouddb/main.sql
+++ clouddb/main.sql
@@ -6,7 +6,7 @@ CREATE DATABASE clouddb;
 USE clouddb;
 
 CREATE TABLE logs(
-  ID int NOT NULL Auto_INCREMENT,
+  ID int NOT NULL AUTO_INCREMENT,
   status VARCHAR(50) NOT NULL,
   time VARCHAR(50) NOT NULL
 );
