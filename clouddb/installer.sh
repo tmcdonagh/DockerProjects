95bbf792644176059b1653148648b7b53159ff8d
tmcdonagh <tmcdonagh12@gmail.com>
2019-09-26 08:46:07 -0500

    Added persistent storage

--- clouddb/installer.sh
+++ clouddb/installer.sh
@@ -2,9 +2,9 @@
 sudo docker build -t clouddb .
 sudo docker run \
   --name=clouddb \
+  -v /home/tom/clouddb-data:/var/lib/mysql \
   -e MYSQL_ROOT_PASSWORD=test \
   -d clouddb \
   #-p 2162:3306 \ #Exposes internal port 3306 to external port 2162
-  #-v /home/tom/clouddb_data:/var/lib/mysql \
   #--restart=always \
 
